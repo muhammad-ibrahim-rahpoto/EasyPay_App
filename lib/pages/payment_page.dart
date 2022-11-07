8b801443985c506bce1fd830c43e122e42f6868c
Muhammad Ibrahim <ibrahimrahpoto266@gmail.com>
2022-11-06 20:00:54 -0800

    First Commit

---
+++ lib/pages/payment_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class PaymentPage extends StatelessWidget {
+  const PaymentPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double h = MediaQuery.of(context).size.height;
+    double w = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Container(
+        height: 300,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage("images/paymentbackground.png"))),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/buttons.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+import '../component/colors.dart';
+
+class AppButtons extends StatelessWidget {
+  final double? fontSize;
+  final IconData icon;
+  final Function()? onTap;
+  final Color? backgroundColor;
+  final Color? iconColor;
+  final Color? textColor;
+  final String? text;
+
+  const AppButtons(
+      {Key? key,
+      this.fontSize = 20,
+      required this.icon,
+      this.onTap,
+      this.backgroundColor = AppColor.mainColor,
+      this.iconColor = Colors.white,
+      this.textColor = AppColor.mainColor,
+      this.text})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: onTap,
+        child: Column(
+          children: [
+            Container(
+              width: 40,
+              height: 40,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: backgroundColor),
+              child: Icon(
+                icon,
+                size: 30,
+                color: iconColor,
+              ),
+            ),
+            text != null
+                ? Text(
+                    text!,
+                    style: TextStyle(color: textColor, fontSize: 14),
+                  )
+                : Container()
+          ],
+        ));
+  }
+}
